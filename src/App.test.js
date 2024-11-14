1104be1ccebf270cf5fc028b2fc09dfff50703b0
Kylie <notcherry@jinhakapply.com>
2024-11-14 12:23:48 +0900

    test code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Dutch Pay title', () => {
+  render(
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  );
+  const titleElement = screen.getByText(/Dutch Pay/i);
+  expect(titleElement).toBeInTheDocument();
 });
---
+++ src/component/CreateGroup.spec.jsx
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
+import CreateGroup from './CreateGroup';
+import userEvent from '@testing-library/user-event';
+
+const renderComponent = () => {
+    render(
+        <RecoilRoot>
+            <CreateGroup />
+        </RecoilRoot>
+    );
+
+    const input = screen.getByPlaceholderText('2022제주도 여행');
+    const saveButton = screen.getByText('저장');
+    const errorMessage = screen.queryByText('그룹 이름을 입력해주세요.');
+
+    return {
+        input,
+        saveButton,
+        errorMessage,
+    };
+};
+
+//CreateGroup.jsx
+describe('그룹 생성 페이지', () => {
+    test('그룹 생성 페이지 렌더링되는가', () => {
+        const { input, saveButton } = renderComponent();
+
+        // todo: input component 테스트
+        expect(input).not.toBeNull();
+        
+        // todo: save button 테스트
+        expect(saveButton).not.toBeNull();
+    });
+
+    test('그룹 이름을 입력하지 않고 "저장"버튼 클릭 시, 에러 메시지 노출', async () => {
+        const { saveButton, errorMessage } = renderComponent();
+
+        await userEvent.click(saveButton);
+        expect(errorMessage).toHaveAttribute('data-validated', 'false');
+    });
+
+    test('그룹 이름을 입력 후, "저장"버튼 클릭 시, 저장 성공', async () => {
+        const { input, saveButton, errorMessage } = renderComponent();
+
+        await userEvent.type(input, '예시 그룹명');
+        await userEvent.click(saveButton);
+
+        expect(errorMessage).toHaveAttribute('data-validated', 'true');
+    });
+});
