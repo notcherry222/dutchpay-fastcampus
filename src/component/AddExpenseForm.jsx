828aabfba85a792ea859e04f4a8384c1d162cfed
Kylie <notcherry@jinhakapply.com>
2024-11-19 17:24:35 +0900

    styling

--- src/component/AddExpenseForm.jsx
+++ src/component/AddExpenseForm.jsx
@@ -4,6 +4,8 @@ import { useRecoilValue } from "recoil";
 import { groupMembersState } from "../state/groupMembers";
 import { expensesState } from "../state/expenses";
 import { useSetRecoilState } from "recoil";
+import styled from "styled-components";
+import { Row, Col } from "react-bootstrap";
 
 export const AddExpenseForm = () => {
     const today = new Date();
@@ -50,67 +52,129 @@ export const AddExpenseForm = () => {
 
     return (
         <div>
-            <Form noValidate onSubmit={handleSubmit}>
-                <h3>1. 비용 추가</h3>
-                <Form.Group>
-                    <Form.Control
-                        type="date"
-                        name="expenseDate"
-                        placeholder="결제한 날짜를 선택해 주세요"
-                        value={date}
-                        onChange={(event) => setDate(event.target.value)}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control
-                        type="text"
-                        name="expenseDescription"
-                        isValid={isDescValid}
-                        isInvalid={!isDescValid && validated}
-                        placeholder="비용에 대한 설명을 입력해 주세요 "
-                        value={desc}
-                        onChange={(event) => setDesc(event.target.value)}
-                    />
-                    <Form.Control.Feedback 
-                        type="invalid"
-                        data-valid={isDescValid}
-                        >비용 내용을 입력해 주셔야 합니다.</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control
-                        type="number"
-                        name="expenseAmount"
-                        isValid={isAmountValid}
-                        isInvalid={!isAmountValid && validated}
-                        placeholder="비용은 얼마였나요?"
-                        value={amount}
-                        onChange={(event) => setAmount(event.target.value)}
-                    />
-                    <Form.Control.Feedback
-                        type="invalid"
-                        data-valid={isAmountValid}
-                        >금액을 입력해 주셔야 합니다.</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Select
-                        name="expensePayer"
-                        isValid={isPayerValid}
-                        isInvalid={!isPayerValid && validated}
-                        onChange={(event) => setPayer(event.target.value)}
-                    >
-                        <option disabled value="">누가 결제했나요?</option>
-                        {members.map((member) => (
-                            <option key={member} value={member}>{member}</option>
-                        ))}
-                    </Form.Select>
-                    <Form.Control.Feedback 
-                        type="invalid"
-                        data-valid={isPayerValid}
-                        >결제자를 선택해 주셔야 합니다.</Form.Control.Feedback>
-                </Form.Group>
-                <Button type="submit">추가하기</Button>
-            </Form>
+            <StyledWrapper>
+                <Form noValidate onSubmit={handleSubmit}>
+                    <h3>1. 비용 추가</h3>
+                    <Row>
+                        <Col xs={12}>
+                            <Form.Group>
+                                <Form.Control
+                                    type="date"
+                                    name="expenseDate"
+                                    placeholder="결제한 날짜를 선택해 주세요"
+                                    value={date}
+                                    onChange={(event) => setDate(event.target.value)}
+                            />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12}>
+                            <Form.Group>
+                                <Form.Control
+                                type="text"
+                                name="expenseDescription"
+                                isValid={isDescValid}
+                                isInvalid={!isDescValid && validated}
+                                placeholder="비용에 대한 설명을 입력해 주세요 "
+                                value={desc}
+                                onChange={(event) => setDesc(event.target.value)}
+                            />
+                            <Form.Control.Feedback 
+                                type="invalid"
+                                data-valid={isDescValid}
+                                >비용 내용을 입력해 주셔야 합니다.</Form.Control.Feedback>
+                                
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                        <Row>   
+                            <Col xs={12} lg={6}>
+                                <Form.Group>
+                                    <Form.Control
+                                        type="number"
+                                        name="expenseAmount"
+                                        isValid={isAmountValid}
+                                        isInvalid={!isAmountValid && validated}
+                                        placeholder="비용은 얼마였나요?"
+                                        value={amount}
+                                        onChange={(event) => setAmount(event.target.value)}
+                                    />
+                                <Form.Control.Feedback
+                                    type="invalid"
+                                    data-valid={isAmountValid}
+                                    >금액을 입력해 주셔야 합니다.</Form.Control.Feedback>
+                                </Form.Group>
+                            </Col>
+                            <Col xs={12} lg={6}>
+                                <Form.Group>
+                                    <Form.Select
+                                        name="expensePayer"
+                                        isValid={isPayerValid}
+                                        isInvalid={!isPayerValid && validated}
+                                        onChange={(event) => setPayer(event.target.value)}
+                                    >
+                                        <option disabled value="">누가 결제했나요?</option>
+                                        {members.map((member) => (
+                                            <option key={member} value={member}>{member}</option>
+                                        ))}
+                                    </Form.Select>
+                                    <Form.Control.Feedback 
+                                    type="invalid"
+                                    data-valid={isPayerValid}
+                                    >결제자를 선택해 주셔야 합니다.</Form.Control.Feedback>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                    <Row>
+                        <StyledSubmitButton>추가하기</StyledSubmitButton>
+                    </Row>
+                </Form>
+            </StyledWrapper>
         </div>
     );
 };
 
+const StyledWrapper = styled.div`
+    padding: 50px;
+    background-color: #683BA1;
+    box-shadow: 3px 8px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 15px;
+
+    input, select {
+        border: none;
+        background-color: #59359A;
+        box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+        color: #F8F9FA;
+        margin-bottom: 18px;
+        height: 45px;
+
+        &:hover{
+            color: #F8F9FA;
+            background-color: #59359A;
+            filter: brightness(80%);
+        }
+
+        ::placeholder {
+            color: #F8F9FA;
+        }
+    }
+`;
+
+const StyledSubmitButton = styled(Button).attrs({
+    type: 'submit',
+})`
+    width: 100%;
+    height: 60px;
+    padding: 16px 32px;
+    border: none;
+    border-radius: 8px;
+    background-color: #E2D9F3;
+    color: #59359A;
+    margin-top: 10px;
+
+    &:hover {
+        background-color: #E2D9F3;
+        filter: brightness(70%);
+    }
+`;
